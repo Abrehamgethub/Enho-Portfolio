ba1afe1d8a609078a6ef3616231add4fff20a538
Abrehamgethub <47387034+Abrehamgethub@users.noreply.github.com>
2026-04-05 14:14:41 +0300

    chore: deployment handshake marker VERIFIED_04052026

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* DEPLOYMENT_HANDSHAKE: VERIFIED_04052026 */}
+        {children}
+      </body>
     </html>
   )
 }
