f95c424ff653973bcdf6720ec306d93d66dfe7a6
Abrehamgethub <47387034+Abrehamgethub@users.noreply.github.com>
2026-05-21 19:46:18 +0300

    chore: implement security hardening best practices (report-only CSP & rotate local secret)

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
             value: 'strict-origin-when-cross-origin',
           },
           {
-            key: 'Content-Security-Policy',
+            key: 'Content-Security-Policy-Report-Only',
             value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.youtube.com https://s.ytimg.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://firebasestorage.googleapis.com https://i.ytimg.com https://img.youtube.com https://yt3.ggpht.com; frame-src 'self' https://www.youtube.com; connect-src 'self' https://firestore.googleapis.com https://identitytoolkit.googleapis.com https://securetoken.googleapis.com https://firebasestorage.googleapis.com https://vitals.vercel-insights.com;",
           },
         ],
